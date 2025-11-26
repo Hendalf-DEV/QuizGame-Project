d3a030b506aba7900ddb76f42c7121cf6f7765d4
DR.Hendalf <800046788@omnia.fi>
2025-11-26 13:00:01 +0200

    feat: add QuizPage component and export in quiz feature module
    - created `QuizPage` component with placeholder content for future quiz functionality
    - exported `QuizPage` from `index.js` in the quiz feature directory

---
+++ src/features/quiz/index.js
@@ -0,0 +1,2 @@
+export { default as QuizPage } from './pages/QuizPage'
+
---
+++ src/features/quiz/pages/QuizPage.jsx
@@ -0,0 +1,11 @@
+const QuizPage = () => {
+  return (
+    <div>
+      <h1>Quiz Game</h1>
+      <p>Quiz functionality will be implemented here.</p>
+      {/* TODO: Implement quiz logic, questions, scoring, etc. */}
+    </div>
+  )
+}
+
+export default QuizPage
