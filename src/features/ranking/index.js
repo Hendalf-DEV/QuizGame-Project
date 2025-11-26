429f4504c957d4db0f7ffe2073db706fe3084b9d
DR.Hendalf <800046788@omnia.fi>
2025-11-26 13:00:07 +0200

    feat: add RankingPage component and export in ranking feature module
    - created `RankingPage` component with placeholder content for future ranking functionality
    - exported `RankingPage` from `index.js` in the ranking feature directory

---
+++ src/features/ranking/index.js
@@ -0,0 +1,2 @@
+export { default as RankingPage } from './pages/RankingPage'
+
---
+++ src/features/ranking/pages/RankingPage.jsx
@@ -0,0 +1,11 @@
+const RankingPage = () => {
+  return (
+    <div>
+      <h1>Rankings</h1>
+      <p>Player rankings will be displayed here.</p>
+      {/* TODO: Implement ranking logic, leaderboards, etc. */}
+    </div>
+  )
+}
+
+export default RankingPage
